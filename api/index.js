40038f28b7d799130cf70890948fa9cb136a1dc1
RAKUMING <papepi.tz@gmail.com>
2025-04-03 15:44:30 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const fetch = require("node-fetch");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+// Configurar CORS para permitir todas las conexiones
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
     res.send("Servidor funcionando en Vercel");
@@ -14,10 +16,24 @@ app.get("/liquidaciones", async (req, res) => {
         const url = "https://api.coinalyze.net/v1/liquidation-history?api_key=84bd6d2d-4045-4b53-8b61-151c618d4311&symbols=BTCUSDT_PERP.A&interval=1hour&from=1742968800&to=1743659999&convert_to_usd=false";
         const response = await fetch(url);
         const data = await response.json();
+
+        // Agregar encabezados CORS manualmente
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
         res.json(data);
     } catch (error) {
         res.status(500).json({ error: "Error al obtener los datos" });
     }
+});
+
+// Habilitar preflight requests (opcional pero recomendado)
+app.options("*", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.send();
 });
 
 module.exports = app;
